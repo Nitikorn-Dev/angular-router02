836d24e247f4aa46ebcc6abb2ff9f56108c1e6ac
Nitikorn-Dev <nitikorn.aiamart@gmail.com>
2021-03-17 20:26:19 +0700

    1 file modified

--- src/app/shopping/product/product.component.ts
+++ src/app/shopping/product/product.component.ts
@@ -1,16 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { ProductService } from '../product.service';
+import { Component, OnInit } from "@angular/core";
+import { ProductsService } from "../products.service";
 
 @Component({
-  selector: 'app-product',
-  templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  selector: "app-product",
+  templateUrl: "./product.component.html",
+  styleUrls: ["./product.component.css"]
 })
 export class ProductComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }

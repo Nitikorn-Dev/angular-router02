7ee2c90cd4156855d86a6d05626c6b647e3c1fdb
Nitikorn-Dev <nitikorn.aiamart@gmail.com>
2021-03-24 20:00:13 +0700

    1 file modified

--- src/app/shopping/products.service.ts
+++ src/app/shopping/products.service.ts
@@ -1,3 +1,4 @@
+import { error } from "@angular/compiler/src/util";
 import { Injectable } from "@angular/core";
 import { Observable, of, from, BehaviorSubject } from "rxjs";
 import { Product, product } from "../mock/product.mock";
@@ -6,18 +7,41 @@ import { Product, product } from "../mock/product.mock";
 export class ProductsService {
 
   constructor() {
-   
+   this.Products2()
   }
 
   get Products(): Observable<Product[]> {
     return of(product);
   }
 
-   Products2(){}
+
+
+resolt:any;
+
+   Products2(){
+
+    this.resolt = this.local.subscribe(this.myObserver)
+
+    console.log(this.resolt)
+   }
+
+myObserver = 
+  {
+   next:(res)=>{return res},
+   error:(err)=>console.error(err),
+   complete:()=>console.log('Complete')
+  }
+
+  
+
+
 
   local = new Observable((observer)=>{
-    observer.next(1);
-    observer.next(2);
+    observer.next('A')
+    observer.next('B')
+    observer.error(55)
+    observer.next('C')
+    observer.complete()
   })
 
 

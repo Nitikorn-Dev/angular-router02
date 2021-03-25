6c69d72cb2258c2e2f652e2e0eca687a7bbbb033
Nitikorn-Dev <nitikorn.aiamart@gmail.com>
2021-03-25 22:39:13 +0700

    3 files modified

--- src/app/shopping/product/product.component.ts
+++ src/app/shopping/product/product.component.ts
@@ -21,8 +21,10 @@ export class ProductComponent implements OnInit {
     );
   }
 
-  ngOnInit() {
-   
+  ngOnInit() {}
 
-  }
+  
+
+
+
 }
--- src/app/shopping/products.service.ts
+++ src/app/shopping/products.service.ts
@@ -1,53 +1,19 @@
 import { error } from "@angular/compiler/src/util";
 import { Injectable } from "@angular/core";
-import { Observable, of, from, BehaviorSubject } from "rxjs";
+import { Observable, of, from, BehaviorSubject, observable } from "rxjs";
 import { Product, product } from "../mock/product.mock";
 
 @Injectable({ providedIn: "root" })
 export class ProductsService {
-
   constructor() {
-   this.Products2()
+    this.Products2();
   }
 
   get Products(): Observable<Product[]> {
     return of(product);
   }
 
+  resolt: any;
 
-
-resolt:any;
-
-   Products2(){
-
-    this.resolt = this.local.subscribe(this.myObserver)
-
-    console.log(this.resolt)
-   }
-
-myObserver = 
-  {
-   next:(res)=>{return res},
-   error:(err)=>console.error(err),
-   complete:()=>console.log('Complete')
-  }
-
-  
-
-
-
-  local = new Observable((observer)=>{
-    observer.next('A')
-    observer.next('B')
-    // observer.error(55)
-    observer.next('C')
-    observer.complete()
-  })
-
-
-  
-
-
-
-
+  Products2() {}
 }
